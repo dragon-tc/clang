70eb21941fa5cadf7a9446df396eed93b5a004fb
Alexander Kornienko <alexfh@google.com>
2015-06-22 09:47:44 +0000

    Fixed/added namespace ending comments using clang-tidy. NFC
    The patch is generated using this command:
    
      $ tools/extra/clang-tidy/tool/run-clang-tidy.py -fix \
          -checks=-*,llvm-namespace-comment -header-filter='llvm/.*|clang/.*' \
          work/llvm/tools/clang
    
    To reduce churn, not touching namespaces spanning less than 10 lines.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@240270 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/CodeGen/TargetInfo.cpp
+++ lib/CodeGen/TargetInfo.cpp
@@ -710,7 +710,7 @@ public:
   }
 };
 
-}
+} // namespace
 
 /// Rewrite input constraint references after adding some output constraints.
 /// In the case where there is one output and one input and we add one output,
@@ -1775,7 +1775,7 @@ void WinX86_64TargetCodeGenInfo::setTargetAttributes(const Decl *D,
 
   addStackProbeSizeTargetAttribute(D, GV, CGM);
 }
-}
+} // namespace
 
 void X86_64ABIInfo::postMerge(unsigned AggregateSize, Class &Lo,
                               Class &Hi) const {
@@ -3176,7 +3176,7 @@ public:
   }
 };
 
-}
+} // namespace
 
 llvm::Value *PPC32_SVR4_ABIInfo::EmitVAArg(llvm::Value *VAListAddr,
                                            QualType Ty,
@@ -3456,7 +3456,7 @@ public:
   }
 };
 
-}
+} // namespace
 
 // Return true if the ABI requires Ty to be passed sign- or zero-
 // extended to 64 bits.
@@ -4002,7 +4002,7 @@ public:
 
   bool doesReturnSlotInterfereWithArgs() const override { return false; }
 };
-}
+} // namespace
 
 ABIArgInfo AArch64ABIInfo::classifyArgumentType(QualType Ty) const {
   Ty = useFirstFieldIfTransparentUnion(Ty);
@@ -4641,7 +4641,7 @@ void WindowsARMTargetCodeGenInfo::setTargetAttributes(
   ARMTargetCodeGenInfo::setTargetAttributes(D, GV, CGM);
   addStackProbeSizeTargetAttribute(D, GV, CGM);
 }
-}
+} // namespace
 
 void ARMABIInfo::computeInfo(CGFunctionInfo &FI) const {
   if (!getCXXABI().classifyReturnType(FI))
@@ -5219,7 +5219,7 @@ void NVPTXTargetCodeGenInfo::addNVVMMetadata(llvm::Function *F, StringRef Name,
   // Append metadata to nvvm.annotations
   MD->addOperand(llvm::MDNode::get(Ctx, MDVals));
 }
-}
+} // namespace
 
 //===----------------------------------------------------------------------===//
 // SystemZ ABI Implementation
@@ -5260,7 +5260,7 @@ public:
     : TargetCodeGenInfo(new SystemZABIInfo(CGT, HasVector)) {}
 };
 
-}
+} // namespace
 
 bool SystemZABIInfo::isPromotableIntegerType(QualType Ty) const {
   // Treat an enum type as its underlying type.
@@ -5570,7 +5570,7 @@ public:
                            CodeGen::CodeGenModule &M) const override;
 };
 
-}
+} // namespace
 
 void MSP430TargetCodeGenInfo::setTargetAttributes(const Decl *D,
                                                   llvm::GlobalValue *GV,
@@ -5652,7 +5652,7 @@ public:
     return SizeOfUnwindException;
   }
 };
-}
+} // namespace
 
 void MipsABIInfo::CoerceToIntArgs(
     uint64_t TySize, SmallVectorImpl<llvm::Type *> &ArgList) const {
@@ -6019,7 +6019,7 @@ void TCETargetCodeGenInfo::setTargetAttributes(
   }
 }
 
-}
+} // namespace
 
 //===----------------------------------------------------------------------===//
 // Hexagon ABI Implementation
@@ -6054,7 +6054,7 @@ public:
   }
 };
 
-}
+} // namespace
 
 void HexagonABIInfo::computeInfo(CGFunctionInfo &FI) const {
   if (!getCXXABI().classifyReturnType(FI))
@@ -6168,7 +6168,7 @@ public:
                            CodeGen::CodeGenModule &M) const override;
 };
 
-}
+} // namespace
 
 void AMDGPUTargetCodeGenInfo::setTargetAttributes(
   const Decl *D,
